d1fdaccf821d953fe574546a6a7614ba02acc626
teramaem51 <teramaem51@gmail.com>
2022-04-04 10:11:02 +0900

    fix -- add input validation and error handling

--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -5,31 +5,20 @@ import (
 	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
-	}
-	for _, tc := range testcases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want%q", rev, tc.want)
-		}
-	}
-}
-
 func FuzzReverse(f *testing.F) {
 	testcases := []string{"Hello, world", " ", "!12345"}
 	for _, tc := range testcases {
 		f.Add(tc) // Use f.Add to provide a seed corpus
 	}
 	f.Fuzz(func(t *testing.T, orig string) {
-		rev := Reverse(orig)
-		doubleRev := Reverse(rev)
-		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
+		}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
 		if orig != doubleRev {
 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
 		}
