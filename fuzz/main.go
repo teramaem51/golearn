f1ad1c95fd2a18476cd46ffab773ebe1e4ca6072
teramaem51 <teramaem51@gmail.com>
2022-04-04 09:52:04 +0900

    fix byte-by-byte to by-runes

--- fuzz/main.go
+++ fuzz/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
 	}
-	return string(b)
+	return string(r)
 }
 
 func main() {
