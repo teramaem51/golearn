d816f4887a42b5bf34229f36c1d3a9ecf5e7cd2f
teramaem51 <teramaem51@gmail.com>
2022-04-04 09:26:35 +0900

    add fuzzing test

--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"unicode/utf8"
+)
 
 func TestReverse(t *testing.T) {
 	testcases := []struct {
@@ -16,4 +19,21 @@ func TestReverse(t *testing.T) {
 			t.Errorf("Reverse: %q, want%q", rev, tc.want)
 		}
 	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
