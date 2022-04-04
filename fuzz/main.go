60abfc6cbf8bf56380507ef18e8b369cc59ac8d2
teramaem51 <teramaem51@gmail.com>
2022-04-04 10:12:37 +0900

    mv main fn to top

--- fuzz/main.go
+++ fuzz/main.go
@@ -6,6 +6,15 @@ import (
 	"unicode/utf8"
 )
 
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+}
+
 func Reverse(s string) (string, error) {
 	if !utf8.ValidString(s) {
 		return s, errors.New("input is not valid UTF-8")
@@ -15,13 +24,4 @@ func Reverse(s string) (string, error) {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r), nil
-}
-
-func main() {
-	input := "The quick brown fox jumped over the lazy dog"
-	rev, revErr := Reverse(input)
-	doubleRev, doubleRevErr := Reverse(rev)
-	fmt.Printf("original: %q\n", input)
-	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
-	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
 }
